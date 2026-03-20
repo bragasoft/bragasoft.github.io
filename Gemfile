0a5aa1cc0c1ddf58b5d48780e44fcfc4d36cbf4a
Santiago Bragagnolo <santiagobragagnolo@gmail.com>
2026-03-20 16:59:55 +0100

    New

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source "https://rubygems.org"
+
+gem "jekyll", "~> 4.3"
+gem "jekyll-redirect-from"
+gem "jekyll-seo-tag"
+gem "jekyll-sitemap"
+
+group :jekyll_plugins do
+  gem "jekyll-feed"
+end
--- assets/js/accordion.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const triggers = document.querySelectorAll('.accordion-header');
-  triggers.forEach(t => {
-    t.addEventListener('click', () => {
-      const body = t.nextElementSibling;
-      body.classList.toggle('show');
-    });
-  });
-});
